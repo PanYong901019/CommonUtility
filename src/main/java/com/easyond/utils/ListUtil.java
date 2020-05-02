09c6153d1fc725b1bfeac59d83fb603d6f0aa584
Pan丶小勇 <pan_panyong@sina.com>
2020-05-02 13:57:40 +0800

    新增ListUtil 增加 两个均分List的方法

---
+++ src/main/java/com/easyond/utils/ListUtil.java
@@ -0,0 +1,47 @@
+package com.easyond.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pan on 2020/5/2 1:55 PM
+ */
+public class ListUtil {
+
+    /**
+     * 将一个list均分成n个size尽量平均的list
+     *
+     * @param source
+     * @return
+     */
+    public static <T> List<List<T>> averageAssignList(List<T> source, int n) {
+        List<List<T>> result = new ArrayList<>();
+        int number = source.size() / n, remaider = source.size() % n;
+        int fromIndex = 0;
+        for (int i = 0; i < n; i++) {
+            int toIndex = (--remaider >= 0 ? (number + 1) * (i + 1) : fromIndex + number);
+            System.out.println(fromIndex + "," + toIndex);
+            result.add(source.subList(fromIndex, toIndex));
+            fromIndex = toIndex;
+        }
+        return result;
+    }
+
+    /**
+     * 将一个list均分成多个size为n的list
+     *
+     * @param source
+     * @return
+     */
+    public static <T> List<List<T>> averageAssignItem(List<T> source, int n) {
+        List<List<T>> result = new ArrayList<>();
+        int length = source.size(), num = (length + n - 1) / n;//多少组
+        int fromIndex = 0;
+        for (int i = 0; i < num; i++) {
+            int toIndex = (i + 1) * n < length ? (i + 1) * n : length;
+            result.add(source.subList(fromIndex, toIndex));
+            fromIndex = toIndex;
+        }
+        return result;
+    }
+}
